fb5d2217e8b07ce2ec1f0e7e9be719311479a110
Pedro Torruella <5025399+ptorru@users.noreply.github.com>
2024-03-28 15:07:12 +0000

    updated and added prompts

--- prompter.py
+++ prompter.py
@@ -1,16 +1,39 @@
 from openai import OpenAI
 import streamlit as st
+import json
+
+PROMPTS = json.load(open("prompts.json"))
 
 with st.sidebar:
-    openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
-    "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
-    "[View the source code](https://github.com/streamlit/llm-examples/blob/main/Chatbot.py)"
-    "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
+    # Add a button to this sidebar to change the system prompt
+    st.markdown("## Choose a mode")
+    # The following button changes the system prompt on the first element of the messages state
 
-st.title("💬 Chatbot")
+    if st.button(f"Base system"):
+        st.session_state.messages = [
+            {"role": "system", "content": "You are a helpful assistant."},
+        ]
+        st.session_state.model = "nous-hermes-2-mixtral-8x7b-dpo"
+
+    for prompt in PROMPTS:
+        if st.button(prompt["name"]):
+            st.session_state.messages = [
+                {"role": "system", "content": prompt["prompt"]},
+            ]
+            st.session_state.model = prompt["model"]
+
+    openai_api_key = st.text_input(
+        "OctoAI API Key", key="chatbot_api_key", type="password"
+    )
+    "[View the source code](https://github.com/ptorru/prompter/blob/main/prompter.py)"
+
+
+st.title("💬 Prompter")
 st.caption("🚀 A streamlit chatbot powered by OpenAI LLM")
 if "messages" not in st.session_state:
-    st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
+    st.session_state["messages"] = [
+        {"role": "assistant", "content": "How can I help you?"}
+    ]
 
 for msg in st.session_state.messages:
     st.chat_message(msg["role"]).write(msg["content"])
@@ -23,7 +46,9 @@ if prompt := st.chat_input():
     client = OpenAI(api_key=openai_api_key, base_url="https://text.octoai.run/v1")
     st.session_state.messages.append({"role": "user", "content": prompt})
     st.chat_message("user").write(prompt)
-    response = client.chat.completions.create(model="nous-hermes-2-mixtral-8x7b-dpo", messages=st.session_state.messages)
+    response = client.chat.completions.create(
+        model=st.session_state.model, messages=st.session_state.messages
+    )
     msg = response.choices[0].message.content
     st.session_state.messages.append({"role": "assistant", "content": msg})
     st.chat_message("assistant").write(msg)
